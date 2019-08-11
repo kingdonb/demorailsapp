625ac53d99d04e21ccd1a9b4e37a526f16adae07
Kingdon Barrett <kingdon.b@nd.edu>
2019-08-11 17:57:31 -0600

    also metrics

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/healthz', to: proc { [200, {}, ['']] }
+  get '/metrics', to: proc { [200, {}, ['']] }
 end
