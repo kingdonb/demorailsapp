aca6140cab3b724f8d5a395e4859c81f9e3f330c
Kingdon Barrett <kingdon.b@nd.edu>
2019-08-12 11:22:05 -0600

    add example controller with index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'example/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/healthz', to: proc { [200, {}, ['']] }
   get '/metrics', to: proc { [200, {}, ['']] }
+
+  root 'example#index'
 end
